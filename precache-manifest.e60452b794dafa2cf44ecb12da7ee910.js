12399c8781f05b5cf615f5e98696f07c27babeb4
Alankhoang <alankhoangfr@gmail.com>
2019-05-21 22:33:06 +0200

    Updates

---
+++ precache-manifest.e60452b794dafa2cf44ecb12da7ee910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d632d531753d7b1c94de36878c5fd5a",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "9989b812cb5f655b79b1",
+    "url": "/shopping/static/css/2.81b134f3.chunk.css"
+  },
+  {
+    "revision": "745abda173e449bf552f",
+    "url": "/shopping/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "9989b812cb5f655b79b1",
+    "url": "/shopping/static/js/2.2369afcd.chunk.js"
+  },
+  {
+    "revision": "745abda173e449bf552f",
+    "url": "/shopping/static/js/main.a4e39ebd.chunk.js"
+  },
+  {
+    "revision": "dd21bce8e1ee6f4128a4",
+    "url": "/shopping/static/js/runtime~main.c6044c27.js"
+  }
+]);
